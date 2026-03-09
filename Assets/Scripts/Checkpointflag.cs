5eeb6ee4b7af407c79983edade1b3df4612eadc9
MingThong2001 <2014ooimingthong@gmail.com>
2026-03-09 20:07:47 +0800

    Documentation fo checkpointflag

--- Assets/Scripts/Checkpointflag.cs
+++ Assets/Scripts/Checkpointflag.cs
@@ -1,46 +1,58 @@
-// ============================================================
-//  CheckpointFlag.cs
-// ============================================================
 using UnityEngine;
 
+//Checkpoint flag behaviour. Handle flag collection and missed flag.
 public class CheckpointFlag : MonoBehaviour
 {
-    public GameObject collectEffectPrefab;
-
+    //Unique id assigned by the manager.
     [HideInInspector] public int flagID = -1;
 
+    //Prevent the flag to be collected more than once.
     private bool collected = false;
+
+    //Ensure the missed event is only reported once.
     private bool missedReported = false;
 
     void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log("[CheckpointFlag] Hit by: " + col.name + " tag: " + col.tag);
 
+        //Stop if is already colelcted.
         if (collected) return;
+
+        //Only active during gameplay.
         if (GameStateManager.GameState != GameState.Playing) return;
+
+        //Only the player can collect the flag.
         if (!col.CompareTag("Flappy")) return;
 
+        //Avoid duplicated collection.
         if (CheckpointManager.Instance != null &&
             CheckpointManager.Instance.IsFlagAlreadyCollected(flagID)) return;
 
         collected = true;
 
-        // Pass the whole player GameObject so manager can snapshot everything
+        //Send the player game object to the manager to save the checkpoint data.
         if (CheckpointManager.Instance != null)
             CheckpointManager.Instance.SaveCheckpoint(col.gameObject, flagID);
 
-        if (collectEffectPrefab != null)
-            Instantiate(collectEffectPrefab, transform.position, Quaternion.identity);
-
+        //Destroy the flag after collection.    
         Destroy(gameObject);
     }
 
     void Update()
     {
+        //Ignore the flag if collected.
         if (collected) return;
+
+        //Check during gameplay only.
         if (GameStateManager.GameState != GameState.Playing) return;
 
+
+        //Convert flag position to viewport coordinates.
         Vector3 screenPos = Camera.main.WorldToViewportPoint(transform.position);
+
+        // If the flag has moved past the left edge of the screen
+        // it means the player missed the checkpoint
         if (screenPos.x < 0f)
         {
             ReportMissed();
@@ -50,10 +62,12 @@ public class CheckpointFlag : MonoBehaviour
 
     void OnDestroy()
     {
+        // If the flag was not collected and not already reported missed
         if (!collected && !missedReported && GameStateManager.GameState == GameState.Playing)
             ReportMissed();
     }
 
+    // Sends a "checkpoint missed" notification to the CheckpointManager
     void ReportMissed()
     {
         if (missedReported) return;
